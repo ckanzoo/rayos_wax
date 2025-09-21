6cd11513fcf29e3167d24e545aeffab7d12ec00d
ckanzoo <pickanzo@gmail.com>
2025-09-21 13:59:20 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'sql12.freesqldatabase.com',
+    'hostname'	=> 'localhost',
     'port'		=> '3306',
-    'username'	=> 'sql12799459',
-    'password'	=> 'rxyLX6y76k',
-    'database'	=> 'sql12799459',
+    'username'	=> 'root',
+    'password'	=> '',
+    'database'	=> 'mockdata',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
